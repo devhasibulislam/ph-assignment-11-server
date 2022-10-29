2cb3b8cfdaeace3351bdfdfff0c512d81052c039
hasibulislam999 <hasib143sl@gmail.com>
2022-10-29 21:21:40 +0600

    driver segment modified

--- index.js
+++ index.js
@@ -1,215 +1,228 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-var jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+var jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // just for avoid warning!
 const avoidWarning = (req) => {
-    console.log(req.route);
+  console.log(req.route);
 };
 
 // adding next level security to db api conduct with ui
-function verifyJWY(req, res, next) {
-    const authHeader = req.headers.authorization;
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
 
-    if (!authHeader) {
-        return res.status(401).send({ message: 'unauthorized access' });
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.PRIVATE_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
     }
-
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.PRIVATE_KEY, (err, decoded) => {
-        if (err) {
-            return res.status(403).send({ message: 'forbidden access' });
-        }
-        // console.log(decoded);
-        req.decoded = decoded;
-        next();
-    })
-
-    // console.log("inside verifyJWT", authHeader);
+    req.decoded = decoded;
+    next();
+  });
 }
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uumdg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uumdg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try {
-        await client.connect();
+  try {
+    await client.connect();
 
-        // own following database
-        const productCollection = client.db("warehouseManagement").collection("product");
-        const myItemsCollection = client.db("warehouseManagement").collection("myItems");
-        const qnaCollection = client.db("warehouseManagement").collection("qna");
-        console.log('MongoDB connected!');
+    // own following database
+    const productCollection = client
+      .db("warehouseManagement")
+      .collection("product");
+    const myItemsCollection = client
+      .db("warehouseManagement")
+      .collection("myItems");
+    const qnaCollection = client.db("warehouseManagement").collection("qna");
+    console.log("Warehouse connected with MongoDB!");
 
-        // use AUTH for extra security for login
-        app.post('/login', async (req, res) => {
-            const user = req.body;
-            const token = jwt.sign(user, process.env.PRIVATE_KEY, {
-                expiresIn: "7d"
-            });
+    // use AUTH for extra security for login
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.PRIVATE_KEY, {
+        expiresIn: "1h",
+      });
 
-            res.send({ token });
-        });
+      res.send({ token });
+    });
 
-        // get all products
-        app.get('/product', async (req, res) => {
-            const pageNumber = parseInt(req.query.pageNumber);
-            const viewItems = parseInt(req.query.viewItems);
+    // get all products
+    app.get("/product", async (req, res) => {
+      const pageNumber = parseInt(req.query.pageNumber);
+      const viewItems = parseInt(req.query.viewItems);
 
-            const query = {};
-            const cursor = productCollection.find(query);
-            let products;
+      const query = {};
+      const cursor = productCollection.find(query);
+      let products;
 
-            if (pageNumber || viewItems) {
-                products = await cursor.skip(pageNumber * viewItems).limit(viewItems).toArray();
-            } else {
-                products = await cursor.toArray();
-            }
+      if (pageNumber || viewItems) {
+        products = await cursor
+          .skip(pageNumber * viewItems)
+          .limit(viewItems)
+          .toArray();
+      } else {
+        products = await cursor.toArray();
+      }
 
-            res.send(products);
-        });
+      res.send(products);
+    });
 
-        // get my added product
-        app.get('/myItems', async (req, res) => {
-            avoidWarning(req);
+    // get my added product
+    app.get("/myItems", async (req, res) => {
+      avoidWarning(req);
 
-            const query = {};
-            const cursor = myItemsCollection.find(query);
-            const products = await cursor.toArray();
+      const query = {};
+      const cursor = myItemsCollection.find(query);
+      const products = await cursor.toArray();
 
-            res.send(products);
-        });
+      res.send(products);
+    });
 
-        // get my added product as myItems product's view
-        // user secured by email and password
-        app.get('/order', verifyJWY, async (req, res) => {
-            const decodedEmail = req.decoded.email;
-            const email = req.query.email;
-            if (email === decodedEmail) {
-                const query = { email };
-                const cursor = myItemsCollection.find(query);
-                const orders = await cursor.toArray();
+    // get my added product as myItems product's view
+    // user secured by email and password
+    app.get("/order", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      if (email === decodedEmail) {
+        const query = { email };
+        const cursor = myItemsCollection.find(query);
+        const orders = await cursor.toArray();
 
-                res.send(orders);
-            } else {
-                res.status(403).send({ message: 'forbidden access' });
-            }
-        });
+        res.send(orders);
+      } else {
+        res.status(403).send({ message: "forbidden access" });
+      }
+    });
 
-        // get my added product as myItems product's view
-        // only profiled by google.com
-        app.get('/item', async (req, res) => {
-            const email = req.query.email;
-            const query = { email };
-            const cursor = myItemsCollection.find(query);
-            const orders = await cursor.toArray();
+    // get my added product as myItems product's view
+    // only profiled by google.com
+    app.get("/item", async (req, res) => {
+      const email = req.query.email;
+      const query = { email };
+      const cursor = myItemsCollection.find(query);
+      const orders = await cursor.toArray();
 
-            res.send(orders);
-        })
+      res.send(orders);
+    });
 
-        // count all products
-        app.get('/productCount', async (req, res) => {
-            avoidWarning(req);
+    // count all products
+    app.get("/productCount", async (req, res) => {
+      avoidWarning(req);
 
-            const query = {};
-            const cursor = productCollection.find(query);
-            const productCount = await cursor.count();
+      const query = {};
+      const cursor = productCollection.find(query);
+      const productCount = await cursor.count();
 
-            res.send({ count: productCount }); // 1. same as 2 but form as object:, output => 'count': 50
-            // res.json(count); // 2. same as 1 but form as json, output => 50
-        });
+      res.send({ count: productCount }); // 1. same as 2 but form as object:, output => 'count': 50
+      // res.json(count); // 2. same as 1 but form as json, output => 50
+    });
 
-        // count all custom product
-        app.get('/itemsCount', async (req, res) => {
-            avoidWarning(req);
+    // count all custom product
+    app.get("/itemsCount", async (req, res) => {
+      avoidWarning(req);
 
-            const query = {};
-            const cursor = myItemsCollection.find(query);
-            const itemsCount = await cursor.count();
+      const query = {};
+      const cursor = myItemsCollection.find(query);
+      const itemsCount = await cursor.count();
 
-            res.send({ count: itemsCount });
-        });
+      res.send({ count: itemsCount });
+    });
 
-        // add a custom product
-        app.post('/addProduct', async (req, res) => {
-            const doc = req.body;
-            const result = await myItemsCollection.insertOne(doc);
+    // add a custom product
+    app.post("/addProduct", async (req, res) => {
+      const doc = req.body;
+      const result = await myItemsCollection.insertOne(doc);
 
-            res.send(result);
-        });
+      res.send(result);
+    });
 
-        // delete a custom product
-        app.delete('/myItems/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await myItemsCollection.deleteOne(query);
+    // delete a custom product
+    app.delete("/myItems/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await myItemsCollection.deleteOne(query);
 
-            res.send(result);
-        });
+      res.send(result);
+    });
 
-        // update a product's segment
-        app.put('/product/:id', async (req, res) => {
-            const updateProduct = req.body;
-            const id = req.params.id;
-            const filter = { _id: ObjectId(id) };
-            const option = { upsert: true };
-            const updateDoc = {
-                $set: updateProduct
-            };
+    // update a product's segment
+    app.put("/product/:id", async (req, res) => {
+      const updateProduct = req.body;
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const option = { upsert: true };
+      const updateDoc = {
+        $set: updateProduct,
+      };
 
-            const result = await productCollection.updateOne(filter, updateDoc, option);
+      const result = await productCollection.updateOne(
+        filter,
+        updateDoc,
+        option
+      );
 
-            res.send(result);
-        });
+      res.send(result);
+    });
 
-        // update a custom product's segment
-        app.put('/myItems/:id', async (req, res) => {
-            const updateProduct = req.body;
-            const id = req.params.id;
-            const filter = { _id: ObjectId(id) };
-            const option = { upsert: true };
-            const updateDoc = {
-                $set: updateProduct
-            };
+    // update a custom product's segment
+    app.put("/myItems/:id", async (req, res) => {
+      const updateProduct = req.body;
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const option = { upsert: true };
+      const updateDoc = {
+        $set: updateProduct,
+      };
 
-            const result = await myItemsCollection.updateOne(filter, updateDoc, option);
+      const result = await myItemsCollection.updateOne(
+        filter,
+        updateDoc,
+        option
+      );
 
-            res.send(result);
-        });
+      res.send(result);
+    });
 
-        // get qna
-        app.get('/qna', async (req, res) => {
-            const query = {};
-            const cursor = qnaCollection.find(query);
-            const blog = await cursor.toArray();
+    // get qna
+    app.get("/qna", async (req, res) => {
+      const query = {};
+      const cursor = qnaCollection.find(query);
+      const blog = await cursor.toArray();
 
-            res.send(blog);
-        });
-
-    } finally {
-        // await client.close()
-    }
+      res.send(blog);
+    });
+  } finally {
+    // await client.close()
+  }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    avoidWarning(req);
+app.get("/", (req, res) => {
+  avoidWarning(req);
 
-    res.send('Warehouse Management at server side running!');
-})
+  res.send("Warehouse Management at server side running!");
+});
 
 app.listen(port, () => {
-    console.log('Warehouse is listening at port: ', port);
-})
+  console.log("Warehouse is listening at port: ", port);
+});
